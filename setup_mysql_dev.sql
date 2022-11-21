cd334fe87340645933c148bdcc698d54da81632f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-21 09:45:52 -0300

    try2

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- Active: 1667584254284@@127.0.0.1@3306@pepe
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON 'hbnb_dev_db'.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
