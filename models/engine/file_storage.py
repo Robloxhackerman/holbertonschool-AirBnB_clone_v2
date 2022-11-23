2086fdb2401e86f4b1222191b7669fc29f1c28be
m1dnas <felipesandimguliermone@gmail.com>
2022-11-22 20:20:18 -0800

    task 5 v3

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,5 +63,5 @@ class FileStorage:
         """Delete storage dictionary from file"""
         if obj == None:
             return
-        if obj in __objects:
-            del __objects[obj]
+        if obj in FileStorage.__objects:
+            del FileStorage.__objects[obj]
