d800efb738af2a6067da59fc193ca37bb5a62d68
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-23 10:17:44 -0300

    try1

--- console.py
+++ console.py
@@ -115,15 +115,31 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        
+        param = args.split(" ")
+        diccionarito = {}
+
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif param[1] not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
+        
+        for PEPE1 in range(1, len(param)):
+            key, valorin = tuple(param[PEPE1].split("="))
+            if valorin[0] == '"':
+                valorin = valorin.split('"').replace(" ", "_")
+            else:
+                valorin = eval(valorin)
+
+            diccionarito[key] = valorin
+        if diccionarito == {}:
+            obj = eval(param[0])()
+        else:
+            obj = eval(param[0])(**diccionarito)
+            storage.new(obj)
+        print(obj.id)
         storage.save()
 
     def help_create(self):
