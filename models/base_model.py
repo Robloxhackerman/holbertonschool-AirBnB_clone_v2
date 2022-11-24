a67f7a8936351555519e8ba7bc7c00f63a5a28bd
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-24 13:19:48 -0300

    opaa

--- models/base_model.py
+++ models/base_model.py
@@ -12,8 +12,8 @@ class BaseModel:
     """A base class for all hbnb models"""
     
     id = Column(String, nullable=False, primary_key=True)
-    created_at = Column(datetime, nullable=False, default=datetime.utcnow())
-    updated_at = Column(datetime, nullable=False, default=datetime.utcnow())
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
