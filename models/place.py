b3e9d379c8ba2a61f65333afe86c8fe8f8e933a7
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-26 18:31:42 -0300

    fix

--- models/place.py
+++ models/place.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = "places"
