a570a739ebf766f5d53c6646d3a6ea39ff8b12e2
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-27 21:30:46 -0300

    trying

--- models/city.py
+++ models/city.py
@@ -3,17 +3,22 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from os import getenv
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
 
-    __tablename__ = "cities"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = "cities"
     
-  
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities")
-        
-    state_id = ""
-    name = ""
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """hmmmm"""
+        super().__init__(*args, **kwargs)
