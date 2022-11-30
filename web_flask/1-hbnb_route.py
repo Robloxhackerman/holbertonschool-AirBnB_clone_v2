03b93831f306132b5bd33dd9656c07e2e29cdc1f
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-30 10:21:28 -0300

    try1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def holinga():
+    return("Hello HBNB")
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return("HBNB")
+
+app.run(host="0.0.0.0")
