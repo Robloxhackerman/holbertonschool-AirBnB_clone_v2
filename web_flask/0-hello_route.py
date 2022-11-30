270ddc9d30a736d8982e3be6f32018b129d4d435
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-30 11:11:28 -0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,12 +3,12 @@
 from flask import Flask
 
 app = Flask(__name__)
-#piporipopaporepo
+
 
 @app.route('/', strict_slashes=False)
 def holinga():
     """aaaa"""
     return("Hello HBNB!")
-#aaaaaaaaaaaaaa
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
