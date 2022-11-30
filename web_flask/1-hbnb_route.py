4027854c147e360eb7b81f441db4dc5cf18d954a
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-30 11:17:21 -0300

    pepocho

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,14 +4,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def holinga():
     """aaaa"""
     return("Hello HBNB")
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """aaaa"""
     return("HBNB")
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
+    
