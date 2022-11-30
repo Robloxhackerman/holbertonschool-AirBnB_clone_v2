6fd7cef2e0709ca34dbe636c321b67b6f967a943
Alejandro Montenegro <zeituniannicolas27@gmail.com>
2022-11-30 11:40:05 -0300

    comments

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -8,21 +8,25 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def holinga():
+    """aaaa"""
     return("Hello HBNB")
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """aaaa"""
     return("HBNB")
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text=None):
+    """aaaa"""
     return "C " + str(text).replace("_", " ")
 
 
 @app.route('/python/<text>', strict_slashes=False)
 def c(text="is cool"):
+    """aaaa"""
     return "Python " + str(text).replace("_", " ")
 
 
